68909cabeabe525f0ddc3fe305395523159dea4d
Luis Gerardo López Vásquez <grillo.chino@hotmail.com>
2019-09-05 11:11:48 -0500

    Image Loader
    Create list images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy
 import os 
 import re
+import PIL
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -14,3 +15,33 @@ from keras.layers.advanced_activations import LeakyReLU
 
 
 
+dirName = os.path.join(os.getcwd(), 'assets\\media\\car_data\\train')
+imgPath = dirName + os.sep
+
+images = []
+directories = []
+directoriesCount = []
+prevRoot = ''
+count = 0
+
+for root, dirnames, filenames in os.walk(imgPath):
+    for filename in filenames:
+        if re.search("\.(jpg|JPEG|jpeg|png|bmp|tiff)$",filename):
+            filePath = os.path.join(root, filename)
+            image = plt.imread(filePath)
+            images.append(image)
+            count += 1
+
+            if prevRoot != root:
+                print(root,count)
+                prevRoot = root
+                directories.append(root)
+                directoriesCount.append(count)
+                count = 0
+directoriesCount.append(count)
+
+directoriesCount = directoriesCount[1:]
+directoriesCount[0] = directoriesCount[0] + 1
+print(f'Directorios leidos: {len(directories)}')
+print(f'Imagenes en directorios: {directoriesCount}')
+print(f'Imagenes totales: {sum(directoriesCount)}')
